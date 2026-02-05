fd00ce8d25695202930fa4769f54471b4c7f6729
Gerran1 <germanvasanov@gmail.com>
2026-02-05 20:30:33 +0700

    добавлена плитка с мясом

---
+++ Assets/Scripts/StoveCounter.cs
@@ -0,0 +1,179 @@
+using System;
+using UnityEngine;
+using static CuttingCounter;
+
+public class StoveCounter : BaseCounter
+{
+    public enum State
+    {
+        Idle,
+        Frying,
+        Fried,
+        Burned
+    }
+
+    public event EventHandler<OnStateChangedEventArgs> OnStateChanged;
+
+    public class OnStateChangedEventArgs : EventArgs
+    {
+        public State state;
+    }
+
+    [SerializeField]
+    private FryingRecipeSO[] fryingRecipeSOArray;
+    [SerializeField]
+    private BurningRecipeSO[] burningRecipeSOArray;
+
+    private float fryingTimer;
+    private float burningTimer;
+    private FryingRecipeSO fryingRecipeSO;
+    private BurningRecipeSO burningRecipeSO;
+    private State state;
+
+    private void Start()
+    {
+        state = State.Idle;
+
+        OnStateChanged?.Invoke(this, new OnStateChangedEventArgs
+        {
+            state = this.state
+        });
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                break;
+            case State.Frying:
+                fryingTimer += Time.deltaTime;
+
+                if (fryingTimer > fryingRecipeSO.fryingTimerMax)
+                {
+                    // пожарено
+                    fryingTimer = 0f;
+                    Debug.Log("Пожарили!");
+                    GetKitchenObject().DestroySelf();
+                    KitchenObject.SpawnKitchenObject(fryingRecipeSO.output, this);
+
+                    state = State.Fried;
+                    burningTimer = 0f;
+                    burningRecipeSO = GetBurningRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+
+                    OnStateChanged?.Invoke(this, new OnStateChangedEventArgs
+                    {
+                        state = this.state
+                    });
+                }
+                break;
+            case State.Fried:
+                burningTimer += Time.deltaTime;
+
+                if (burningTimer > burningRecipeSO.burningTimerMax)
+                {
+                    GetKitchenObject() .DestroySelf();
+                    KitchenObject.SpawnKitchenObject(burningRecipeSO.output, this);
+
+                    Debug.Log("Сгорело!");
+                    state = State.Burned;
+
+                    OnStateChanged?.Invoke(this, new OnStateChangedEventArgs
+                    {
+                        state = this.state
+                    });
+                }
+                break;
+            case State.Burned:
+                break;
+            default:
+                break;
+        }
+        Debug.Log(state);
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            //на тумбе ничего не лежит
+            if (player.HasKitchenObject())
+            {
+                if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    //у игрока есть объект в руках
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+
+                    fryingRecipeSO = GetFryingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+                    state = State.Frying;
+                    fryingTimer = 0f;
+
+                    OnStateChanged?.Invoke(this, new OnStateChangedEventArgs
+                    {
+                        state = this.state
+                    });
+                }
+            }
+        }
+        else
+        {
+            /*на тумбе уже лежит предмет*/
+            if (player.HasKitchenObject())
+            {
+                //у игрока есть объект в руках
+            }
+            else
+            {
+                //у игрока ничего нет в руках
+                GetKitchenObject().SetKitchenObjectParent(player);
+
+                state = State.Idle;
+                OnStateChanged?.Invoke(this, new OnStateChangedEventArgs
+                {
+                    state = this.state
+                });
+            }
+        }
+    }
+
+    /// <summary>
+    /// принимает объект для нарезки и достает из рецепта результат нарезки
+    /// </summary>
+    /// <param name="inputKitchenObjectSO">обьект для нарезки</param>
+    /// <returns>нарезанная версия</returns>
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSOWithInput(inputKitchenObjectSO);
+        return fryingRecipeSO.output;
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSOWithInput(inputKitchenObjectSO);
+        return fryingRecipeSO != null;
+    }
+
+    private FryingRecipeSO GetFryingRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (FryingRecipeSO fryingRecipeSO in fryingRecipeSOArray)
+        {
+            if (fryingRecipeSO.input == inputKitchenObjectSO)
+            {
+                return fryingRecipeSO;
+            }
+        }
+        return null;
+    }
+
+    private BurningRecipeSO GetBurningRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (BurningRecipeSO burningRecipeSO in burningRecipeSOArray)
+        {
+            if (burningRecipeSO.input == inputKitchenObjectSO)
+            {
+                return burningRecipeSO;
+            }
+        }
+        return null;
+    }
+}
